0696a463ba8d2309cc0679799d9b18b9a546f8a7
Lwin Moe Aung <123534766+DenialLewis@users.noreply.github.com>
2023-09-12 10:19:59 +0000

    done

--- src/main/java/th/mfu/ConcertController.java
+++ src/main/java/th/mfu/ConcertController.java
@@ -1,63 +1,53 @@
 package th.mfu;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-
-import org.springframework.beans.propertyeditors.CustomDateEditor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ConcertController {
-    // TODO: create hashmap of concerts for storing data
 
-    //TODO: add initbinder to convert date
+    private HashMap<Integer, Concert> concerts = new HashMap<Integer, Concert>();
+    private int nextId = 1;
 
     @GetMapping("/concerts")
     public String listConcerts(Model model) {
-        // TODO: add concerts to model
-        // TODO: return a template to list concerts
-        return "";
+        // List<Concert> concerts = new ArrayList<>(concertMap.values());
+        model.addAttribute("concerts", concerts.values());
+        return "list-concert";
     }
 
     @GetMapping("/add-concert")
     public String addAConcertForm(Model model) {
-        // TODO: pass blank concert to a form
-        // TODO: return a template for concert form
-        return "";
+        model.addAttribute("concert", new Concert());
+        return "add-concert-form";
     }
 
     @PostMapping("/concerts")
     public String saveConcert(@ModelAttribute Concert concert) {
-        // TODO: add concert to list of concerts
-        // TODO: increment nextId
-        // TODO: redirect to list concerts
-        return "";
+        concert.setId(nextId);
+        concerts.put(nextId, concert);
+        nextId++;
+        return "redirect:/concerts";
     }
 
     @GetMapping("/delete-concert/{id}")
     public String deleteConcert(@PathVariable int id) {
-        // TODO: remove concert from list of concerts
-        // TODO: redirect to list concerts
-        return "";
+        concerts.remove(id);
+        return "redirect:/concerts";
     }
 
-    
     @GetMapping("/delete-concert")
     public String removeAllConcerts() {
-        //TODO: clear all employees and reset id
-        // TODO: redirect to list concerts
-        return "";
+        concerts.clear();
+        nextId = 1;
+        return "redirect:/concerts";
     }
 
 }
